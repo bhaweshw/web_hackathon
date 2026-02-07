8d46a763ec8c3750444917f2a1851e47b80dcd3c
hijacker-01 <kushagragupta.072725@kgpian.iitkgp.ac.in>
2026-02-08 02:34:48 +0530

    commit

--- backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require("../controllers/auth.controllers");
-
-const router = express.Router();
-
-// POST /api/auth/register - Register a new user
-router.post("/register", authController.registerUser);
-
-module.exports = router;  
