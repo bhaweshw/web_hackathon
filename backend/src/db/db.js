947990805e8818a6a01cc41d6c778abdfc1a8080
hijacker-01 <kushagragupta.072725@kgpian.iitkgp.ac.in>
2026-02-07 13:26:34 +0530

    commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("DB is connected successfully")
+    } catch(err) {
+        console.error("DB connection error:", err)
+    }
+}
+
+module.exports = connectDB;
